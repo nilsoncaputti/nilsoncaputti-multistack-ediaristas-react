1cf34601673c4ab60da7b037589942c6f302b020
nilsoncaputti <nilson_caputti@hotmail.com>
2022-02-24 16:06:32 -0300

    Corrigindo instalação incorreta pacote Pagar.me

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '83ca18f99427938b41b0c645bebe1e38c88ba951',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '83ca18f99427938b41b0c645bebe1e38c88ba951',
+            'dev_requirement' => false,
+        ),
+    ),
+);
